abcde81a45def1aafa78a939223250183747d0b7
Animesh Solanki <92034910+AnimeshSolanki@users.noreply.github.com>
2023-10-12 11:06:53 +0530

    Time: 17 ms (84.88%), Space: 6.8 MB (30.63%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c || board[i][col] == c) {
+                return false;
+            }
+        }
+
+        int subgridRow = 3 * (row / 3);
+        int subgridCol = 3 * (col / 3);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[subgridRow + i][subgridCol + j] == c) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+};
