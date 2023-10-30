9a232d99bbcdad9659f48caef35a32ddc48bf2e7
Animesh Solanki <92034910+AnimeshSolanki@users.noreply.github.com>
2023-10-30 10:42:37 +0530

    Time: 15 ms (15.69%), Space: 10.4 MB (66.29%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    int Count(int n){
+        int c = 0;
+        while(n >0){
+            if(n&1){
+                c++;
+            }
+            n>>=1;
+        }
+        return c;
+    }
+
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [](int a, int b) {
-            int countA = __builtin_popcount(a); // Count set bits in a
-            int countB = __builtin_popcount(b); // Count set bits in b
+        sort(arr.begin(), arr.end(), [this](int a, int b) {
+            int countA = Count(a);
+            int countB = Count(b);
 
             if (countA == countB) {
-                return a < b; // If set bit counts are the same, sort in ascending order
+                return a < b;
             }
 
-            return countA < countB; // Sort by the number of set bits
+            return countA < countB;
         });
-
         return arr;
     }
 };
